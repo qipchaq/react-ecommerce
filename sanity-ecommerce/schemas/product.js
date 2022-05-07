ee5e56d461c15c7fdfa4aba47d59531e2ab85252
Adil Nurkali <a.nurkaliyev@platform.live>
2022-05-07 17:26:08 +0200

    set up sanity

---
+++ sanity-ecommerce/schemas/product.js
@@ -0,0 +1,22 @@
+export default {
+    name: 'product',
+    title: 'Product',
+    type: 'document',
+    fields: [
+        {
+            name: 'image',
+            title: 'Image',
+            type: 'array',
+            of: [{ type: 'image' }],
+            options: {
+                hotspot: true,
+            }
+        },
+        {
+            name: 'name',
+            title: 'Name',
+            type: 'string',
+        },
+
+    ]
+}
